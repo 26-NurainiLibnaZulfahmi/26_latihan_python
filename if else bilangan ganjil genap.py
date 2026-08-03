f3741028eb61dd07664c6c8767fe3d6ed00ec1e1
26-NurainiLibnaZulfahmi <ami.tkjsmeksa25@gmail.com>
2026-08-03 14:34:39 +0700

    Add files via upload

---
+++ if else bilangan ganjil genap.py
@@ -0,0 +1,29 @@
+
+
+print("------------------------------------------")
+
+print("==========================================")
+print("                 IF ELSE                  ")
+print("==========================================")
+
+a = 10
+b = 100
+if b > a:
+    print("b Lebih besar dari a")
+else:
+    print("b lebih kecil dari a")
+    
+    
+nilai = 75
+if nilai >= 70:
+    print("Lulus")
+else:
+    print("Tidak lulus")
+    
+    
+    
+bilangan = int(input("Masukkan bilangan: "))
+ganjil_genap = "Bilangan ganjil" if bilangan % 2 !=0 else "Bilangan genap"
+print("Bilangan", bilangan, "adalah", ganjil_genap)
+
+    
