76b3e30c422552c120bc6e1465b71f870996dd98
26-NurainiLibnaZulfahmi <ami.tkjsmeksa25@gmail.com>
2026-08-10 14:35:08 +0700

    v2 bilangan ganjil genap.py

--- if else bilangan ganjil genap.py
+++ if else bilangan ganjil genap.py
@@ -1,12 +1,7 @@
 while True:
     print("\n--- CEK BILANGAN GANJIL ATAU GENAP ---")
-    
-    # Meminta pengguna untuk memasukkan angka
     angka = int(input("Masukkan angka: "))
-    
-    # Memeriksa apakah angka habis dibagi 2 atau tidak
     if angka % 2 == 0:
-        print(f"{angka} adalah bilangan genap")  
+        print(f"angka {angka} adalah bilangan genap")  
     else:
-        print(f"{angka} adalah bilangan ganjil")
-    
+        print(f"angka {angka} adalah bilangan ganjil")
