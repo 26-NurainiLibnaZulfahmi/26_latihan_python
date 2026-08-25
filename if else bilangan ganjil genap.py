3976843cf3280cce6520817004e5dad31eeb4c70
26-NurainiLibnaZulfahmi <ami.tkjsmeksa25@gmail.com>
2026-08-25 10:12:25 +0700

    2 program

--- if else bilangan ganjil genap.py
+++ if else bilangan ganjil genap.py
@@ -1,3 +1,9 @@
+bilangan = int(input("Masukkan bilangan: "))
+ganjil_genap = "Bilangan ganjil" if bilangan % 2 !=0 else "Bilangan genap"
+print("Bilangan", bilangan, "adalah", ganjil_genap)
+
+print("--------------------------------------------")
+
 while True:
     print("\n--- CEK BILANGAN GANJIL ATAU GENAP ---")
     angka = int(input("Masukkan angka: "))
