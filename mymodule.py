eae49ebc9e1643e9ed9864cc5ed515e872292338
26-NurainiLibnaZulfahmi <ami.tkjsmeksa25@gmail.com>
2026-08-31 14:25:17 +0700

    apdet

--- mymodule.py
+++ mymodule.py
@@ -16,16 +16,11 @@ def cek_bilangan_prima(angka):
 
 
 def huruf_besar(kalimat):
-    kalimat = input ("Masukkan kalimat dengan huruf kecil: ")
-    print("Kalimat dalam huruf besar:", huruf_besar(kalimat))
     return kalimat.upper()
 
 
 def huruf_kecil(kalimat):
-    kalimat = input ("Masukkan kalimat dengan huruf besar: ")
-    print("Kalimat dalam huruf kecil:", huruf_kecil(kalimat))
     return kalimat.lower()
-
 
 
 
