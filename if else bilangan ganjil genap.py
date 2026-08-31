448f65fef624c89ee61a8e68c46d1607d60bcb8e
26-NurainiLibnaZulfahmi <ami.tkjsmeksa25@gmail.com>
2026-08-31 14:40:55 +0700

    tambahan

--- if else bilangan ganjil genap.py
+++ if else bilangan ganjil genap.py
@@ -26,12 +26,12 @@ while True:
 print("--------------------------------------------")
 
 
-def cek_ganjil_genap(angka):
+ef cek_ganjil_genap(angka):
     if angka % 2 == 0:
         return f"{angka} adalah bilangan Genap."
     else:
         return f"{angka} adalah bilangan Ganjil."
-
+    
 
 def cek_bilangan_prima(angka):
     if angka < 2:
@@ -40,7 +40,6 @@ def cek_bilangan_prima(angka):
     for i in range(2, int(angka**0.5) + 1):
         if angka % i == 0:
             return f"{angka} bukan bilangan prima."
-
     return f"{angka} adalah bilangan Prima."
 
 
@@ -55,26 +54,42 @@ def huruf_kecil(kalimat):
 print("--------------------------------------------")
 
 
+import mymodule
+
+print(mymodule.cek_bilangan_prima(7))
+print(mymodule.cek_ganjil_genap(14))
+print(mymodule.huruf_besar("halo semua"))
+print(mymodule.huruf_kecil("HALO SEMUA"))
+
+
 def main():
     while True:
         print("\n=== MENU PROGRAM ===")
         print("1. Ganjil Genap")
         print("2. Bilangan Prima")
-        print("3. Exit")
+        print("3. Huruf Besar")
+        print("4. Huruf Kecil")
+        print("5. Exit")
 
-        pilihan = input("Pilih menu diatas (1-3): ")
+        pilihan = input("Pilih menu (1-5): ")
 
         if pilihan == "1":
             angka = int(input("Masukkan angka: "))
-            print(cek_ganjil_genap(angka))
+            print(mymodule.cek_ganjil_genap(angka))
         elif pilihan == "2":
             angka = int(input("Masukkan angka: "))
-            print(cek_bilangan_prima(angka))
+            print(mymodule.cek_bilangan_prima(angka))
         elif pilihan == "3":
-            print("Program selesai. Terima kasih!")
+            kalimat = input("Masukkan kalimat: ")
+            print(mymodule.huruf_besar(kalimat))
+        elif pilihan == "4":
+            kalimat = input("Masukkan kalimat: ")
+            print(mymodule.huruf_kecil(kalimat))
+        elif pilihan == "5":
+            print("Terima kasih! Program selesai.")
             break
         else:
-            print("Pilihan tidak valid. Coba lagi")
+            print("Pilihan tidak valid. Silakan coba lagi.")
 
 
 if __name__ == "__main__":
